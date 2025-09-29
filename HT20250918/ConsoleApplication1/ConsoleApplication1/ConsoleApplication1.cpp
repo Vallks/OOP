c85fc0e642cbcf9cf738a6daf45cf4ffd808b929
Валентин Командиров <komandirov.v@mail.ru>
2025-09-29 20:00:25 +0300

    Add

---
+++ HT20250918/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+#include <list>
+#include <stdexcept>
+
+class EmptyListException : public std::exception 
+{
+public:
+    const char* what() const noexcept override 
+    {
+        return "Ошибка: попытка удаления из пустого списка";
+    }
+};
+
+template<typename T>
+class SafeDoublyLinkedList {
+private:
+    std::list<T> data;
+
+    void checkEmpty() const 
+    {
+        if (data.empty()) 
+        {
+            throw EmptyListException();
+        }
+    }
+
+public:
+    void pushBack(const T& value) 
+    {
+        data.push_back(value);
+    }
+
+    void pushFront(const T& value) 
+    {
+        data.push_front(value);
+    }
+
+    void popBack() 
+    {
+        checkEmpty();
+        data.pop_back();
+    }
+
+    void popFront() 
+    {
+        checkEmpty();
+        data.pop_front();
+    }
+
+    void remove(const T& value) 
+    {
+        checkEmpty();
+        data.remove(value);
+    }
+
+    bool empty() const 
+    {
+        return data.empty();
+    }
+
+    size_t size() const 
+    {
+        return data.size();
+    }
+
+    void print() const 
+    {
+        for (const auto& item : data) 
+        {
+            std::cout << item << " <-> ";
+        }
+        std::cout << "NULL" << std::endl;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    SafeDoublyLinkedList<int> list;
+
+    list.pushBack(1);
+    list.pushBack(2);
+    list.pushFront(0);
+    list.print(); 
+
+    list.popFront();
+    list.popBack();
+    list.print(); 
+
+    list.popBack(); 
+    try 
+    {
+        list.popBack(); 
+    }
+    catch (const EmptyListException& e) 
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    return 0;
+}
